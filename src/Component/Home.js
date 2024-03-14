0790897c7db51fa592d2dd0de79ae96def97c3c7
NeelAnghan <163093399+NeelAnghan@users.noreply.github.com>
2024-03-14 10:55:54 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
                                             forecast for free and experience.</p>
                                         <div className='row'>
                                             <div className='col-lg-3 col-12'>
-                                                <button type='button' className="btn1 ms-4"> <i className="fa-brands fa-apple pe-3"></i>APP STORE</button>
+                                                <button type='button' className="btn1"> <i className="fa-brands fa-apple pe-3"></i>APP STORE</button>
                                             </div>
                                             <div className='col-lg-3 col-12 msl-g-3'>
                                                 <button type='button' className="btn1"> <i className="fa-brands fa-google-play pe-3"></i> PLAY STORE</button>
