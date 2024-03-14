7f6ffdd864660ae3f87bb5cb2a721549c420cf34
NeelAnghan <163093399+NeelAnghan@users.noreply.github.com>
2024-03-14 10:48:48 +0530

    Update Home.js

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -15,9 +15,6 @@ export default function Home() {
                                         <p>Use weather application and get weather information daily and daily. Download weather
                                             forecast for free and experience.</p>
                                         <div className='row'>
-                                            // <div className='col-lg-3 col-12'>
-                                            //     <button type='button' className="btn1"><i className="fa-brands fa-apple ico1 pe-3"></i>APP STORE</button>
-                                            // </div>
                                             <div className='col-lg-3 col-12'>
                                                 <button type='button' className="btn1 ms-2"> <i className="fa-brands fa-apple pe-3"></i>APP STORE</button>
                                             </div>
